3f6fa64e0a75b07f41724f6a6a6c65ada63e7ea4
miwakan <kmiwa0920@gmail.com>
2026-08-04 23:10:19 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 const CONFIG = {
   // 公開スプレッドシートのCSV URL（ファイル→共有→ウェブに公開→形式:CSV で発行したもの）
   // 空のままだとサンプルデータが自動表示されます。
-  CSV_URL: 'https://docs.google.com/spreadsheets/d/1bzxod1otVLb_k1rvVnXZlk20oKsKuMQPqyXOlmjXOSI/export?format=csv&gid=1596284126',
+  CSV_URL: 'https://docs.google.com/spreadsheets/d/1bzxod1otVLb_k1rvVnXZlk20oKsKuMQPqyXOlmjXOSI/export?format=csv&gid=1663163122',
 
   // 日時が "8/1 9:00" のように年を省略した書式の場合に使う年
   DEFAULT_YEAR: 2026,
